371a9582d44446e063291b7aa578cc2d07d43390
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-08 12:52:58 +0530

    Refactored: RequestMapping to set URL request mapping to /hello

--- src/main/java/com/bridgelabz/restapimessagingapp/controller/MessageController.java
+++ src/main/java/com/bridgelabz/restapimessagingapp/controller/MessageController.java
@@ -4,22 +4,23 @@ import com.bridgelabz.restapimessagingapp.entity.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/hello")
 public class MessageController {
 
     //         localhost:8080/hello
-    @RequestMapping(value = {"/hello", "/hello/"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     public String showHello() {
         return "Hello from BridgeLabz!\n";
     }
 
     //         localhost:8080/hello/query?name=Mark
-    @RequestMapping(value = {"/hello/query"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String helloQuery(@RequestParam(value = "name") String name) {
         return "Hello " + name + " from BridgeLabz!\n";
     }
 
     //         localhost:8080/hello/param/Mark
-    @GetMapping(value = {"/hello/param/{name}"})
+    @GetMapping(value = {"/param/{name}"})
     public String helloPathParam(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!\n";
     }
@@ -30,13 +31,13 @@ public class MessageController {
 //        "fname" : "Mark",
 //            "lname" : "Taylor"
 //    }
-    @RequestMapping(value = {"/hello/post"}, method = RequestMethod.POST)
+    @RequestMapping(value = {"/post"}, method = RequestMethod.POST)
     public String helloPost(@RequestBody UserDetails user) {
         return user.details();
     }
 
     //    localhost:8080/hello/put/Mark?lname=Taylor
-    @PutMapping("/hello/put/{fname}")
+    @PutMapping("/put/{fname}")
     public String helloPut(@PathVariable String fname, @RequestParam(value = "lname") String lname) {
         return "Hello " + fname + " " + lname + " from BridgeLabz!\n";
     }
