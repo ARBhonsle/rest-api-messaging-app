f75b1b0a66caa6dc6f0f9b17cbb96ac2e6df924c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-08 12:20:50 +0530

    Added: RequestMapping of Request Method(GET) with query parameter: name

--- src/main/java/com/bridgelabz/restapimessagingapp/controller/MessageController.java
+++ src/main/java/com/bridgelabz/restapimessagingapp/controller/MessageController.java
@@ -2,15 +2,21 @@ package com.bridgelabz.restapimessagingapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
     // localhost:8080/hello
-    @RequestMapping(value = {"/hello","/hello/"},method = RequestMethod.GET)
-    public String showHello(){
-        return "Hello from BridgeLabz! \n";
+    @RequestMapping(value = {"/hello", "/hello/"}, method = RequestMethod.GET)
+    public String showHello() {
+        return "Hello from BridgeLabz!\n";
     }
 
+    // localhost:8080/hello/query?name=Mark
+    @RequestMapping(value = {"/hello/query"}, method = RequestMethod.GET)
+    public String helloQuery(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz!\n";
+    }
 }
